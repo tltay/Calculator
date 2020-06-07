eece5f8f839a987b6adba14bbba2cd119dce5238
liting tay <tayliting0108@gmail.com>
2020-06-08 00:11:13 +0800

    Add modulus / power of functionality. Allow user to do the calculation until '=' is hit

--- Calculator.cpp
+++ Calculator.cpp
@@ -2,55 +2,87 @@
 
 #include <iostream>
 #include <string>
+
 using namespace std;
 
 int add(int first, int second) {
-    return first + second;
+	return first + second;
 }
 int subtract(int first, int second) {
-    return first - second;
+	return first - second;
 }
 int multiple(int first, int second) {
-    return first * second;
+	return first * second;
 }
 int division(int first, int second) {
-    return first / second;
+	return first / second;
+}
+int mod(int first, int second) {
+	return first % second;
+}
+int powerOf(int first, int second) {
+	
+	if (second == 0) {
+		return first * first;
+	}
+	else {
+		second -= 1;
+		return powerOf(first, second);
+	}
 }
 
 int doOperation(char operation, int firstNo, int secondNo) {
-    
-    switch (operation) {
-    case '+': 
-        return add(firstNo, secondNo);
-        break;
-    case '-': 
-        return subtract(firstNo, secondNo);
-        break;
-    case '*': 
-        return multiple(firstNo, secondNo);
-        break;
-    case '/': 
-        return division(firstNo, secondNo);
-        break;
-    default: 
-        return 0;
-    }
+
+	switch (operation) {
+	case '+':
+		return add(firstNo, secondNo);
+		break;
+	case '-':
+		return subtract(firstNo, secondNo);
+		break;
+	case '*':
+		return multiple(firstNo, secondNo);
+		break;
+	case '/':
+		return division(firstNo, secondNo);
+		break;
+	case '%':
+		return mod(firstNo, secondNo);
+		break;
+	case '^':
+		return powerOf(firstNo, secondNo);
+	default:
+		return 0;
+	}
 }
 
-int main()
-{
-    int first{};
-    int second{};
-    char operation{};
+int main() {
+	int first{};
+	char operation{};
+	int result{};
 
-    cout << "This is a calculator. " << endl;
-    cout << "Please enter number : ";
-    cin >> first;
-    cout << "Please choose your operation: ";
-    cin >> operation;
-    cout << "Please enter number : ";
-    cin >> second;
+	cout << "This is a calculator which do + - * / % ^" << endl;
 
-    int result = doOperation(operation, first, second);
-    cout << "Ans : " << result << endl;
+	do {
+		cout << "Enter number : ";
+		cin >> first;
+		if (operation != NULL) {
+			cout << result << " " << operation << " " << first;
+
+			result = doOperation(operation, result, first);
+			cout << " = ";			
+		}
+		else {
+			result = first;
+		}
+
+		cout << result;
+		cout << endl;
+		cout << "Enter operation: ";
+		cin >> operation;
+
+	} while (operation != '=');
+
+	cout << "final asnwer = " << result;
+	return 0;
 }
